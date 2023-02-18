c233a5dfdd4e2129bba7fc1affd6cc27cd573e9d
sergeiklein <sjklein088@gmail.com>
2023-02-18 13:07:46 -0800

    Add project files.

---
+++ Personality Quiz/Personality.cs
@@ -0,0 +1,93 @@
+﻿using Personality_Quiz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personality_Quiz
+{
+    public class Personality
+    {
+        private int gandalf;
+        private int gimli;
+        private int gollum;
+        private int treebeard;
+        public int Gandalf { get; set; }
+        public int Gimli { get; set; }
+        public int Gollum { get; set; }
+        public int Treebeard { get; set; }
+
+        public Personality(int gandalf, int gimli, int gollum, int treebeard)
+        {
+            Gandalf = gandalf;
+            Gimli = gimli;
+            Gollum = gollum;
+            Treebeard = treebeard;
+        }
+        public void GandalfScore()
+        { 
+            Gandalf = Gandalf+1;
+        }
+        public void GimliScore()
+        {
+            Gimli = Gimli+1;
+        }
+        public void GollumScore()
+        {
+            Gollum = Gollum+1;
+        }
+        public void TreebeardScore()
+        {
+            Treebeard = Treebeard+1;
+        }
+        public void UserPersonality()
+        {
+            if (Gandalf > Gimli && Gandalf > Gollum && Gandalf > Treebeard)
+            {
+                Console.WriteLine("You must be a wise wizard like Gandalf!");
+            }
+            else if (Gimli > Gandalf && Gimli > Gollum && Gimli > Treebeard)
+            {
+                Console.WriteLine("You must be short and grumpy like Gimli");
+            }
+            else if (Gollum > Gandalf && Gollum > Gimli && Gollum > Treebeard)
+            {
+                Console.WriteLine("You must be aloof and creepy like Gollum");
+            }
+            else
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("You must be old and stronglike Treebeard\n");
+                Console.WriteLine("Your scores:\n");
+                Console.WriteLine("Gandalf =" + Gandalf);
+                Console.WriteLine("Gimli =" + Gimli);
+                Console.WriteLine("Gollum =" + Gollum);
+                Console.WriteLine("Treebeard =" + Treebeard);
+            }
+        }
+        public void Question()
+        {
+
+                string userChoice = Console.ReadLine().ToLower();
+                switch (userChoice)
+                {
+                    case "1":
+                        GandalfScore();
+                        break;
+                    case "2":
+                        GimliScore();
+                        break;
+                    case "3":
+                        GollumScore();
+                        break;
+                    case "4":
+                        TreebeardScore();
+                        break;
+                    default:
+                        break;
+                }
+        }
+
+    }
+}
---
+++ Personality Quiz/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Personality_Quiz;
+using System;
+using System.Xml.Linq;
+
+Console.WriteLine("MIDDLE EARTH PERSONALITY QUIZ\n");
+
+Console.WriteLine("Answer following questions to see which Lord of the Rings character you are most alike.\n");
+
+Personality quizScore = new Personality(0,0,0,0);
+
+Console.WriteLine("Question 1:   If you had to wage war against orcs what would be your weapon of choice?.\n");
+Console.WriteLine("1. Powerful magic staff");
+Console.WriteLine("2. Battle Axe");
+Console.WriteLine("3. Random rock");
+Console.WriteLine("4. Brute Strength");
+quizScore.Question();
+
+Console.WriteLine("Question 2:   If you lived in Middle Earth where would you live?.\n");
+Console.WriteLine("1. In a tall tower");
+Console.WriteLine("2. No place in particular. You prefer to be a travelr and see the world");
+Console.WriteLine("3. In a cave under a mountain");
+Console.WriteLine("4. In a beautiful and magical forest");
+quizScore.Question();
+
+Console.WriteLine("Question 3:   Which of the following would you do in yur downtime?.\n");
+Console.WriteLine("1. Smoke a pipe");
+Console.WriteLine("2. Fishing for some fishes");
+Console.WriteLine("3. Drinking Beer");
+Console.WriteLine("4. Go for a walk");
+quizScore.Question();
+
+Console.WriteLine("Question 4:   What grinds your gears?\n");
+Console.WriteLine("1. When someone is being a fool");
+Console.WriteLine("2. When someone steals from you");
+Console.WriteLine("3. When someone is taller then you");
+Console.WriteLine("4. When someone chops down pretty trees");
+quizScore.Question();
+
+Console.WriteLine("Question 5:   What woud you buy if you had all the gold in the world?\n");
+Console.WriteLine("1. A beautiful hourse or maybe two horses. Like lots of horses.");
+Console.WriteLine("2. You woulnt spend it but hide it somewhere safe.");
+Console.WriteLine("3. A ring!! One pretty ring!");
+Console.WriteLine("4. Plant trees and grow a huge forest!");
+quizScore.Question();
+
+quizScore.UserPersonality();
+
+
